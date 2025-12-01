9354015e1d27f28c61651fee72174877143931c2
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-12-02 07:26:27 +1100

    leetcode 1365

--- Leetcode/FindAllMissingNumbers_448.js
+++ Leetcode/FindAllMissingNumbers_448.js
@@ -12,7 +12,7 @@
 // convert array to a set to remove the duplicates. Because our range is [1, n] each value has to be in the nums array, if its not we push it to the new array.
 // created set of nums, create empty answer array. Iterate through the new set looking for each number 1 to n. if that integer doesn't appear push it to the ans array.
 // Time complexity is O(n) - linear. not optimal time complexity but that low key doesn't matter bc im a rookie lol.
-// Optimal time complexity would be marking the integers in their own place. each integer being between 1 and n helps us because it refernces its own index. we'd iterate through the array mark what indices are present with -1 flipping the value to negative showing they've been visited. We now have our integers that are missing from the array. loop through again and 1 to the index to find the value and push to the answer array. No idea how to code it but there we go
+// Optimal time complexity would be marking the integers in their own place making it O(1). each integer being between 1 and n helps us because it refernces its own index. we'd iterate through the array mark what indices are present with -1 flipping the value to negative showing they've been visited. We now have our integers that are missing from the array. loop through again and 1 to the index to find the value and push to the answer array. No idea how to code it but there we go
 var findDisappearedNumbers = function(nums) {
   const set = new Set(nums);
   const ans = [];
